0e50f80019037f6df54bcb76f4b0856d4c1ef82e
dotan <frenkeldotan1998@gmail.com>
2023-04-13 10:14:24 +0300

    Add footers

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ from fpdf import FPDF
 import pandas as pd
 
 pdf = FPDF(orientation='P', unit='mm', format='A4')   # P for portrait and L for landscape
+pdf.set_auto_page_break(auto=False, margin=0)
 
 df = pd.read_csv("topics.csv")
 for index, row in df.iterrows():
-    print(row['Topic'])
     pdf.add_page()
+
+    # Set the header
     pdf.set_font(family='Times', size=24,
                  style="B")  # every cell that will be created after this will be in the font that is written
     pdf.set_text_color(70, 70, 70)   # RGB - numbers from 0-254
@@ -17,8 +19,20 @@ for index, row in df.iterrows():
     # h - height , "12" is the size of the border which is recommended to be the size of the font
     pdf.line(10, 22, 200, 22)   # line(x1,y1,x2,y2) x represent the distance in mm from the left side of the page and right side , and y represent the height from the top
 
+    # Set the footer
+    pdf.ln(260)   # the height of A4 page is 298 mm so 278 is down the page
+    pdf.set_font(family='Times', size=12, style="B")
+    pdf.set_text_color(180, 180, 180)
+    pdf.cell(w=0, h=10, txt=row['Topic'], align='R')
+
     for i in range(row['Pages']-1):
         pdf.add_page()
+
+        # Set the footer
+        pdf.ln(272)
+        pdf.set_font(family='Times', size=12, style="B")
+        pdf.set_text_color(180, 180, 180)
+        pdf.cell(w=0, h=10, txt=row['Topic'], align='R')
 
 
 pdf.output('output.pdf')   # generates a pdf file into the folder
